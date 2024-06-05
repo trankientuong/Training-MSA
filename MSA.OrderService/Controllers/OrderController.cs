4ba2dbf3ccdec9c6995f6f89131cfe16101de84f
HARVEYNASH\tuong.trankien <tuong.trankien@nashtechglobal.com>
2024-06-06 00:21:37 +0700

    Implement saga-choreography

--- MSA.OrderService/Controllers/OrderController.cs
+++ MSA.OrderService/Controllers/OrderController.cs
@@ -5,6 +5,8 @@ using MSA.OrderService.Dtos;
 using MSA.Common.Contracts.Domain;
 using MSA.Common.PostgresMassTransit.PostgresDB;
 using MSA.OrderService.Services;
+using MassTransit;
+using MSA.Common.Contracts.Domain.Commands.Product;
 
 namespace MSA.OrderService.Controllers;
 
@@ -16,16 +18,20 @@ public class OrderController : ControllerBase
 
     private readonly PostgresUnitOfWork<MainDbContext> uow;
     private readonly IProductService productService;
+    private readonly ISendEndpointProvider sendEndpointProvider;
+
 
     public OrderController(
         IRepository<Order> repository,
         PostgresUnitOfWork<MainDbContext> uow
 ,
-        IProductService productService)
+        IProductService productService,
+        ISendEndpointProvider sendEndpointProvider)
     {
         this.repository = repository;
         this.uow = uow;
         this.productService = productService;
+        this.sendEndpointProvider = sendEndpointProvider;
     }
 
     [HttpGet]
@@ -39,8 +45,8 @@ public class OrderController : ControllerBase
     public async Task<ActionResult<Order>> PostAsync(CreateOrderDto createOrderDto)
     {
         //validate and ensure product exist before creating
-        var isProductExisted = await productService.IsProductExisted(createOrderDto.ProductId);
-        if (!isProductExisted) return BadRequest();
+        // var isProductExisted = await productService.IsProductExisted(createOrderDto.ProductId);
+        // if (!isProductExisted) return BadRequest();
 
         var order = new Order { 
             Id = Guid.NewGuid(),
@@ -50,6 +56,16 @@ public class OrderController : ControllerBase
         await repository.CreateAsync(order);
 
         await uow.SaveChangeAsync();
+
+        //async validate
+        var endpoint = await sendEndpointProvider.GetSendEndpoint(
+            new Uri("queue:product-validate-product")
+        );
+        await endpoint.Send(new ValidateProduct
+        {
+            OrderId = order.Id,
+            ProductId = createOrderDto.ProductId
+        });
 
         return CreatedAtAction(nameof(PostAsync), order);
     }
